dd0af5f0582c8eab93195a9bc9fa71fe75696d5f
NW_DLT <alexputil228@mail.ru>
2022-10-17 14:36:08 +0300

    add docker-compose

--- AnkiBackEnd/obj/Debug/net6.0/AnkiBackEnd.AssemblyInfo.cs
+++ AnkiBackEnd/obj/Debug/net6.0/AnkiBackEnd.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1a1330f4-f3ea-452c-89e4-dc7dcfb3ce76")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnkiBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
