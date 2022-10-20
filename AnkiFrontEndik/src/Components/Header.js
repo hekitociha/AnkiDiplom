99d46a95d5d0b1056f0a66b0decf59253a210020
hekitociha <chernov.200125@gmail.com>
2022-10-20 09:08:58 +0300

    Ну крч добавил запрос на карточки, но он не работает пока

--- AnkiFrontEndik/src/Components/Header.js
+++ AnkiFrontEndik/src/Components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, FormControl, Container, Form, Button  } from 'react-bootstrap'
+import { FormControl, Input, InputLabel,FormHelperText } from '@mui/material';
 import './Style.css'
 
 export default class Header extends Component {
@@ -7,18 +7,14 @@ export default class Header extends Component {
         return (
             <div className="navbar">
                 <div className="navbar left">
-                    <a href="/"><img className="nav-img" src="../money.png" width="70" alt="ico"></img></a>
+                    <a href="/"><img className="nav-img" src="../Anki-icon.svg" width="70" alt="ico"></img></a>
                     <a className="nav-link" href="/">Главная</a>
                     <a className="nav-link" href="/contacts" >Контакты</a>
                 </div>
                 <div className="navbar center">
-                    <Form className="search">
-                        <FormControl
-                            type="text"
-                            placeholder="Поиск"
-                            className="search"
-                        />
-                    </Form>
+                    <FormControl>
+                        <Input id="my-input" aria-describedby="my-helper-text" />
+                    </FormControl>
                 </div>
                 <div className="navbar right">
                     <button className="button signin" onClick>Войти</button>
